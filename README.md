31e901372d0a0dc22e8fed0620e1f75db56fa0e9
arcticwolf666 <c.l.arctos@outlook.com>
2024-11-25 08:37:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# 安倍晋三Discord読み上げボット
+
+Discordのチャンネルに常駐し、投稿されたテキストを読み上げます。
+
+## 使用モデルについて
+    晋バルサン氏の公開しているStyle-Vert-VITS2用のモデルをダウンロードし使用しています。
+    https://huggingface.co/AbeShinzo0708/AbeShinzo_Style_Bert_VITS2
+
+## 使用方法
+1. Pythonの仮想環境を作成し、その中にStyle-Bert-VITS2をインストールします
+
+    ```
+    python -m venv venv
+    venv\Scripts\activate
+    pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+    pip install style-bert-vits2
+    ```
+2. Discord上アプリケーションを作成する
+
+    https://discord.com/developers/applications からアプリケーションを作成する
+    ![1-application.png](images/1-applications.png)
+    ![2-create.png](images/2-create.png)
+    ![3-generalinfo.png](images/3-generalinfo.png)
+    BOTを作成します
+    ![4-createbot.png](images/4-createbot.png)
+    BOTをサーバに追加する為のURLを取得します(GENERATED URL)
+    ![5-oauth.png](images/5-oauth.png)
+    ![6-permission.png](images/6-permission.png)
+    BOTをサーバに追加する、前のGENERATED URLをブラウザで開きサーバにBOTを追加します。
+    ![7-addbot.png](images/7-addbot.png)
+    ![8-auth.png](images/8-auth.png)
+
+3. discordbot.py の TOKEN をDiscordで発行されたBOTトークンに書き換えて下さい。
+
+    ```
+    TOKEN="DiscordのBOTで生成したトークンに書き換える"
+    ```
+4. Pythonの仮想環境上でBOTを動かす
+
+    ```
+    venv\Scripts\activate
+    python discordbot.py
+    ```
+5. 読み上げる
+    Discordのチャンネルで /join とコマンドを打つと、そのチャンネルに読み上げBOTが接続します。<br>
+    以後テキストが読み上げられる筈です。<br>
+    BOTの接続を解除するには /leave とコマンドを打って下さい。<br>
---
+++ discordbot.py
@@ -0,0 +1,80 @@
+import discord
+import os
+from scipy.io import wavfile
+
+# Discordから発行されたBOTのトークンを指定
+TOKEN="XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+TEMPWAVFILE="output.wav"
+
+intents = discord.Intents.none()
+intents.messages = True
+intents.message_content = True
+intents.guilds = True
+intents.voice_states = True
+client = discord.Client(intents=intents)
+
+# BERTモデルをダウンロード
+from style_bert_vits2.nlp import bert_models
+from style_bert_vits2.constants import Languages
+
+bert_models.load_model(Languages.JP, "ku-nlp/deberta-v2-large-japanese-char-wwm")
+bert_models.load_tokenizer(Languages.JP, "ku-nlp/deberta-v2-large-japanese-char-wwm")
+
+# Hugging Faceからモデルをダウンロード
+from pathlib import Path
+from huggingface_hub import hf_hub_download
+
+model_file = "AbeShinzo20240210_e300_s43800.safetensors"
+config_file = "config.json"
+style_file = "style_vectors.npy"
+
+for file in [model_file, config_file, style_file]:
+    print(file)
+    hf_hub_download("AbeShinzo0708/AbeShinzo_Style_Bert_VITS2", file, local_dir="model_assets")
+
+# 上でダウンロードしたモデルファイルを指定して音声合成のテスト
+from style_bert_vits2.tts_model import TTSModel
+
+assets_root = Path("model_assets")
+
+model = TTSModel(
+    model_path=assets_root / model_file,
+    config_path=assets_root / config_file,
+    style_vec_path=assets_root / style_file,
+    device="cpu",
+)
+
+@client.event
+async def on_ready():
+    print(f"Logged in as")
+    print(f"client.user.name: {client.user.name}")
+    print(f"client.user.id: {client.user.id}")
+    print(f"discord.version: {discord.__version__}")
+    print(f'ready...')
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content == "/join":
+        if message.author.voice is None:
+            await message.channel.send("あなたはボイスチャンネルに接続していません。")
+            return
+        # connect to voice channel
+        await message.author.voice.channel.connect()
+        await message.channel.send("接続しました")
+    elif message.content == "/leave":
+        if message.guild.voice_client is None:
+            await message.channel.send("接続していません。")
+            return
+        # disconnect
+        await message.guild.voice_client.disconnect()
+        await message.channel.send("切断しました。")
+    else:
+        # play local wav file.
+        print(f"推論します: {message.content}")
+        sr, audio = model.infer(text=message.content)
+        wavfile.write(TEMPWAVFILE, sr, audio)
+        message.guild.voice_client.play(discord.FFmpegPCMAudio(TEMPWAVFILE))
+
+client.run(TOKEN)
